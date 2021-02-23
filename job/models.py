cf4c6096a8f37316a0ee660f81fda6dea5c93647
turki awadh <turki15.8@gmail.com>
2021-02-23 23:46:27 +0300

    migration files

--- job/models.py
+++ job/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
+
+JOB_TYPE = (
+( 'Full Time', 'Full Time'), 
+( 'Part Time', 'Part Time'), 
+
+
+)
+
 # Create your models here.
+class Job (models.Model):
+    title = models.CharField (max_length=100)
+
+   # location
+Job_type = models.CharField(max_length=15 , choices = JOB_TYPE)
+
+
+
+
+
