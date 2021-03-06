c4d99b31f5331ed53a047eaf6b5715bd5c096800
turki awadh <turki15.8@gmail.com>
2021-03-06 23:52:00 +0300

    add job views& url

--- job/models.py
+++ job/models.py
@@ -13,7 +13,6 @@ JOB_TYPE = (
 # Create your models here.
 class job (models.Model):
     title = models.CharField (max_length=100)
-
    # location
     job_type = models.CharField(max_length=15 , choices = JOB_TYPE)
     descrpition = models.TextField(max_length=1000) 
@@ -21,10 +20,24 @@ class job (models.Model):
     vacany = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    Category = models.ForeignKey('Category',on_delete=models.CASCADE)
 
 
     def __str__  (self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__ (self):
+        return self.name 
+
+
+
+
+
+
 
 
 
--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #'myapp',
-    'job',
+    'job', 
 ]
 
 MIDDLEWARE = [
@@ -79,7 +82,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
